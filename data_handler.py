4cbe8c80506261c3e1d59617768be13a8ebc6c0f
zsoltMeszaros <meszazaolt13@gmail.com>
2019-06-11 19:23:56 +0200

    Please dont be worried, i did some coding.. (everything is different majorly) according to the curriculum, this is what it should mostly look like, i tried my best ;-;

--- data_handler.py
+++ data_handler.py
@@ -10,12 +10,17 @@ def get_answers(id_):
     with open("sample_data/answer.csv", "r") as answer_file:
         readed_answers=csv.DictReader(answer_file)
         for row in readed_answers:
-            if row['question_id']==id_:
+            if row['question_id'] == id_:
                 answer.append(row['message'])
         return answer
 
+def get_post_by_id(id_):
 
+    with open("sample_data/question.csv", "r") as data_file:
+        data = csv.DictReader(data_file)
+        for row in data:
+            if row['id'] == id_:
+                return row
 
-
-
-#get_all_data()
+def make_question_row(title,message):
+    return
--- server.py
+++ server.py
@@ -1,20 +1,46 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 import data_handler
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/list')
 def route_list():
-    questions=data_handler.get_all_data()
+    questions = data_handler.get_all_data()
 
-    return render_template('index.html', questions=questions)
+    return render_template('list.html', questions=questions)
 
-@app.route('/<id_>')
-def route_answer(id_):
-    answer=data_handler.get_answers(id_)
 
-    return render_template("answer.html", answer=answer)
+@app.route('/question/<id_>')
+def route_question_by_id(id_):
+
+    answers = data_handler.get_answers(id_)
+    post = data_handler.get_post_by_id(id_)
+
+    return render_template('question.html', post=post, answers=answers)
+
+
+@app.route('/add-a-question', methods=['GET', 'POST'])
+def route_add_a_question():
+    questions = data_handler.get_all_data()
+    if request.method == "POST":
+        title = request.form["title"]
+        message = request.form["message"]
+        return redirect('/')
+
+    return render_template('add-a-question.html', questions=questions)
+
+
+@app.route('/question/<question_id>/new-answer', methods=["GET", "POST"])
+def route_new_answer(question_id):
+    post = data_handler.get_post_by_id(question_id)
+
+    if request.method == "POST":
+        answer = request.form["answer"]
+        return redirect("/")
+
+    return render_template("new-answer.html", post=post)
 
 
 if __name__ == '__main__':
